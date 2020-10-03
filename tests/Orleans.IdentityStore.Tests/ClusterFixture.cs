db3532686665eaa10c4b01740b7ad3df78ae3eaa
smanickam <smanickam@metocean.com>
2020-10-03 10:35:15 -0400

    Added unit test project

---
+++ tests/Orleans.IdentityStore.Tests/ClusterFixture.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans.Hosting;
+using Orleans.TestingHost;
+using System;
+using Xunit;
+
+namespace Orleans.IdentityStore.Tests
+{
+    [CollectionDefinition(Name)]
+    public class ClusterCollection : ICollectionFixture<ClusterFixture>
+    {
+        public const string Name = "ClusterCollection";
+    }
+
+    public class ClusterFixture : IDisposable
+    {
+        public ClusterFixture()
+        {
+            var builder = new TestClusterBuilder();
+            builder.AddSiloBuilderConfigurator<TestSiloConfigurations>();
+
+            Cluster = builder.Build();
+            Cluster.Deploy();
+        }
+
+        public TestCluster Cluster { get; }
+
+        public void Dispose()
+        {
+            this.Cluster.StopAllSilos();
+        }
+
+        public class TestSiloConfigurations : ISiloConfigurator, IHostConfigurator
+        {
+            public void Configure(ISiloBuilder siloBuilder)
+            {
+                siloBuilder.UserOrleanIdentityStore();
+            }
+
+            public void Configure(IHostBuilder hostBuilder)
+            {
+            }
+        }
+    }
+
+    [CollectionDefinition("ClusterCollection")]
+    public class DummyCollection : ICollectionFixture<string>
+    {
+    }
+}
---
+++ tests/Orleans.IdentityStore.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Orleans.IdentityStore.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
